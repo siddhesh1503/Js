974896c06d8e92026d975ee9c07a0164a9a3bd88
siddhesh1503 <158297148+siddhesh1503@users.noreply.github.com>
2024-07-02 07:25:06 +0000

    data type

---
+++ basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "siddhesh@google.com"
+var accountPassword = "123499"
+accountCity = "Pune"
+
+
+console.log("accountId");
+console.table([accountId, accountEmail,accountPassword, accountCity ])
---
+++ basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //this is use for using newst version of js//
+
+// alert( 3+3) //we are using nodejs not browser//
+ console.log("siddhesh")
+
+ let name = "siddhesh"
+ let age = "18"
+ let isLoggedIn = false
+ let state;
+
+ //number => 2 to power 53
+ // bigint
+ // string => ""
+ //boolean => true/ false
+ // null=> standalone vale
+ // undefined => unique
+
+ console.log(typeof age)

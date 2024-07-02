6c37ee23add856c1d5a1c60146630541b2e3706d
siddhesh1503 <158297148+siddhesh1503@users.noreply.github.com>
2024-07-02 07:41:47 +0000

    conversion datatype

---
+++ basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score = "siddhesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// a33b => NaN
+// true = 1, false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
